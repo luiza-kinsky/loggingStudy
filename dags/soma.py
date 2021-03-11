18b62ac49381e3b8587f4ca7255f154c3a09c79e
luiza-kinsky <luiza@4intelligence.com.br>
2021-03-11 11:28:39 -0300

    adicionando soma.py

---
+++ dags/soma.py
@@ -0,0 +1,59 @@
+import logging
+from pythonjsonlogger import jsonlogger
+
+# Função que determina o json
+def setup_logging(log_level, log_format):
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
+    logHandler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter(log_format)
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
+
+'''
+   A formatação abaixo permite personalizar
+   a forma como o log será mostrado
+'''
+# DateTime:Level:Arquivo:Mensagem
+log_format = '%(asctime)s:%(levelname)s:%(filename)s:%(message)s'
+log_level = logging.DEBUG
+setup_logging(log_level, log_format)
+'''
+   Aqui as configurações do modulo são definidas
+
+   filename = 'nome do arquivo em que ficará salvar a mensagem do log.'
+   filemode = 'a forma em que o arquivo será gravado.'
+   level = 'level em que o log atuará'
+   format = 'formatação da mensagem do log'
+'''
+logging.basicConfig(filename='soma.log',
+                    # w -> sobrescreve o arquivo a cada log
+                    # a -> não sobrescreve o arquivo
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format=log_format)
+
+'''
+   O objeto getLogger() permite o retorno de
+   varias instancias de logs.
+'''
+# Instancia do objeto getLogger()
+logger = logging.getLogger('root')
+
+def add(x: str, y: str) -> str:
+    """
+        Essa função recebe o primeiro nome e o segundo nome de uma pessoa e retorna o nome completo dela
+    """
+
+    # Aqui, é verificado se os parametros passados são do tipo string (str)
+    if isinstance(x, int) and isinstance(y, int):
+        logger.info(f'{x} {y}')
+
+        return x + y
+    else:
+        logger.error(
+            f'{x} type: {type(x)} - {y} type: {type(y)}'
+        )
+
+add(5, 10)
+add('Luiza', True)
